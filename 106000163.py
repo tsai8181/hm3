5cc752f598532575fac65697b88f54f53ccb9ac7
unknown <pandasoemoe@gmail.com>
2018-03-27 15:45:21 +0800

    106000163

---
+++ 106000163.py
@@ -0,0 +1,16 @@
+##106000163
+
+typing=input("Please type anything")
+d={}
+for b in typing:
+    if(b in d):
+        d[b]=(d[b]+1)
+    else:
+        d[b]=1
+word=d.keys()
+number=d.values()
+for item in d.items():
+    word,number=item
+    print("'"+word+"'",":",number)
+
+#https://blog.csdn.net/qq807237096/article/details/68940848
